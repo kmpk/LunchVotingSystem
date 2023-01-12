f30fb7e60e30d1f883dae1145b5284fe32285065
kmpk <kmpk.gm@gmail.com>
2023-01-12 17:49:17 +0400

    Initial commit

---
+++ src/main/java/com/github/lunchvotingsystem/LunchVotingSystemApplication.java
@@ -0,0 +1,11 @@
+package com.github.lunchvotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchVotingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LunchVotingSystemApplication.class, args);
+    }
+}
