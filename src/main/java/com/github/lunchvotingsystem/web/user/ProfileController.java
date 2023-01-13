bac508dc0353af5c38a890cd78bff309ef4a6812
kmpk <kmpk.gm@gmail.com>
2023-01-13 18:52:31 +0400

    Add users profile management

---
+++ src/main/java/com/github/lunchvotingsystem/web/user/ProfileController.java
@@ -0,0 +1,42 @@
+package com.github.lunchvotingsystem.web.user;
+
+import com.github.lunchvotingsystem.model.User;
+import com.github.lunchvotingsystem.to.UserTo;
+import com.github.lunchvotingsystem.util.UsersUtil;
+import com.github.lunchvotingsystem.util.security.AuthUser;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import static com.github.lunchvotingsystem.util.ValidationUtil.assureIdConsistent;
+
+@RestController
+@RequestMapping(value = ProfileController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProfileController extends AbstractUserController {
+    static final String REST_URL = "/api/profile";
+
+    @GetMapping
+    public User get(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("get {}", authUser);
+        return authUser.getUser();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@AuthenticationPrincipal AuthUser authUser) {
+        super.delete(authUser.id());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public void update(@RequestBody @Valid UserTo userTo, @AuthenticationPrincipal AuthUser authUser) {
+        log.info("update {} with id={}", userTo, authUser.id());
+        assureIdConsistent(userTo, authUser.id());
+        User user = authUser.getUser();
+        prepareAndSave(UsersUtil.updateFromTo(user, userTo));
+    }
+}
